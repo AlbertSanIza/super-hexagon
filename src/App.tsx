4335360d0ac011093ebf6fec1bbcb4a9d7e8d732
Albert Sanchez <albert.san.iza@gmail.com>
2025-07-06 12:51:50 -0700

    Remove unused sprite addition functionality from App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,9 @@ import { PhaserGame } from '@/PhaserGame'
 export default function App() {
     const phaserRef = useRef<IRefPhaserGame | null>(null)
 
-    const addSprite = () => {
-        if (phaserRef.current) {
-            const scene = phaserRef.current.scene
-            if (scene) {
-                const x = Phaser.Math.Between(64, scene.scale.width - 64)
-                const y = Phaser.Math.Between(64, scene.scale.height - 64)
-                scene.add.sprite(x, y, 'star')
-            }
-        }
-    }
-
     return (
-        <div id="app">
+        <div className="fixed inset-0 items-center justify-center bg-orange-50">
             <PhaserGame ref={phaserRef} />
-            <div>
-                <div>
-                    <button className="button" onClick={addSprite}>
-                        Add New Sprite
-                    </button>
-                </div>
-            </div>
         </div>
     )
 }
