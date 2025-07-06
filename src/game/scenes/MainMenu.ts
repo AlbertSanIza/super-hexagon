54bcd4f1d8e34311df44d0caf9ad6e4a6f4030ef
Albert Sanchez <albert.san.iza@gmail.com>
2025-07-06 13:03:53 -0700

    Add MainMenu scene with title and start prompt

---
+++ src/game/scenes/MainMenu.ts
@@ -0,0 +1,15 @@
+import { Scene } from 'phaser'
+
+export class Menu extends Scene {
+    constructor() {
+        super('Menu')
+    }
+
+    create() {
+        const centerX = this.cameras.main.width / 2
+        const centerY = this.cameras.main.height / 2
+        this.add.text(centerX, centerY - 100, 'Super Hexagon', { fontSize: '64px', color: '#000000' }).setOrigin(0.5)
+        this.add.text(centerX, centerY, 'Click to Start', { fontSize: '32px', color: '#000000' }).setOrigin(0.5)
+        this.input.on('pointerdown', () => this.scene.start('Game'))
+    }
+}
