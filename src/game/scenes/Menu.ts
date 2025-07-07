44555d920c0c38638cc63faa5eb490a7b05432db
Albert Sanchez <albert.san.iza@gmail.com>
2025-07-06 23:11:42 -0700

    Update menu text styling and positioning for consistency

--- src/game/scenes/Menu.ts
+++ src/game/scenes/Menu.ts
@@ -8,8 +8,8 @@ export class Menu extends Scene {
     create() {
         const centerX = this.cameras.main.width / 2
         const centerY = this.cameras.main.height / 2
-        this.add.text(centerX, centerY - 100, 'Super Hexagon', { fontSize: '64px', color: '#FFFFFF' }).setOrigin(0.5)
-        this.add.text(centerX, centerY, 'Click to Start', { fontSize: '32px', color: '#FFFFFF' }).setOrigin(0.5)
+        this.add.text(centerX, centerY, 'SUPER HEXAGON', { fontSize: '64px', color: '#FFFFFF' }).setOrigin(0.5)
+        this.add.text(centerX, centerY + 40, 'click to start', { fontSize: '18px', color: '#FFFFFF' }).setOrigin(0.5)
         this.input.on('pointerdown', () => this.scene.start('Game'))
     }
 }
