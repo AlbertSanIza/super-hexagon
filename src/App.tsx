e3fcb6af43ce290d547698ab844e7be08be013fb
Albert Sanchez <albert.san.iza@gmail.com>
2025-07-06 18:38:47 -0700

    Update background color in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     const phaserRef = useRef<IRefPhaserGame | null>(null)
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-stone-300">
+        <div className="fixed inset-0 flex items-center justify-center bg-[#0ABAB5]">
             <PhaserGame ref={phaserRef} />
         </div>
     )
